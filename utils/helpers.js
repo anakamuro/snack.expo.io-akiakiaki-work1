8a7296979ab97086b37613eddca6761578734ea0
Akihiko Nakamura <anakamuro@earthlink.net>
2020-11-03 19:30:22 -0500

    Create helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, AsyncStorage } from "react-native";
+import { Notifications, Permissions } from "expo";
+
+const NOTIFICATION_KEY = "flashcards: notifications";
+
+function createNotification() {
+  return {
+    title: "Study Hard",
+    body: "Warning!! Do not forget to study today!",
+    ios: {
+      sound: true
+    }
+  };
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+        .then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(20);
+            tomorrow.setMinutes(0);
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day"
+            });
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
+}
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllSheduleNotificationsAsync
+  );
+}

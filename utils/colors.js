56617a87cd9683499b3c5d08c81d156253173fa7
Akihiko Nakamura <anakamuro@earthlink.net>
2020-11-03 19:29:07 -0500

    Create colors.js

---
+++ utils/colors.js
@@ -0,0 +1,9 @@
+export const purple = "#292477";
+export const white = "#fff";
+export const orange = "#f26f28";
+export const blue = "#4e4cb8";
+export const green = "#62A87c";
+export const red = "#e54B4B";
+export const gold = "#FFD700";
+export const yellow = "#FFFF00";
+export const gray = "#808080"
